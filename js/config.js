57c282a4d519360c89a88cdcda6eb7c39476acd4
mrsmsas <akram.ibrahim@mara.gov.my>
2026-08-25 06:05:09 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================================
 const CONFIG = {
   // URL Web App GAS selepas Deploy > New deployment (Code.gs dalam folder gas-api/)
-  API_URL: 'https://script.google.com/macros/s/AKfycbxTJAPzjpq8DzxT1GBYMzKWrTt0nEOy9k8P85Gb-RRxFFkajMyN5fTAv5FstfVa8LqV/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwWWF5mApDPPYOZHWkiYctvDOFZVqmurEnoNZ-VJWedX95l3u1R2ScIwLDMIHZSvfh-/exec',
 
   // MESTI SAMA dengan API_SECRET dalam Code.gs. Tukar kepada rentetan rawak
   // sendiri (contoh: buka https://www.uuidgenerator.net/ dan salin satu UUID)
